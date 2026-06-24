22c777b3274403231660be7a73cbbcb8f4d59fac
Ayush Mishra <155222263+ayushmishra9061@users.noreply.github.com>
2026-06-24 15:22:07 +0530

    Update env.js

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -6,7 +6,7 @@ export const env = {
   nodeEnv: process.env.NODE_ENV || 'development',
   port: Number(process.env.PORT || 5000),
   clientUrl: process.env.CLIENT_URL || 'https://civicmate.vercel.app,https://civicmate-quyqxfa1b-ayush-mishra-civicmate.vercel.app',
-  mongoUri: process.env.MONGO_URI || 'mongodb+srv://rajamishra1609_db_user:fxq3ledbI5QEaReV@cluster0.qf0tyjc.mongodb.net/civicmate?retryWrites=true&w=majority',
+  mongoUri: process.env.MONGO_URI || 'mongodb+srv://rajamishra1609_db_user:YMUaSCBXivtJw4TP@cluster0.qf0tyjc.mongodb.net/civicmate?retryWrites=true&w=majority',
   jwtAccessSecret: process.env.JWT_ACCESS_SECRET || 'dev-access-secret',
   jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || 'dev-refresh-secret',
   jwtAccessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN || '15m',
